072c04ca5abaae57e9e5869aa5271f18197f5a18
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-04-20 03:23:11 +0600

    error fixed

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -19,14 +19,21 @@ const Navbar = () => {
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item active">
-              {/* <a class="nav-link  ms-5" aria-current="page" href="#">
+            {/* <li class="nav-item active">
+              <a class="nav-link  ms-5" aria-current="page" href="#">
                 Home
-              </a> */}
+              </a>
               <a className="nav-link ms-5" href="#">
                 Home <span className="sr-only">(current)</span>
               </a>
+            </li> */}
+
+            <li className="nav-item">
+              <Link className="nav-link mr-5  " to="dashboard/admittance">
+                Home
+              </Link>
             </li>
+
             <li class="nav-item">
               <a class="nav-link ms-5" href="#">
                 Students
